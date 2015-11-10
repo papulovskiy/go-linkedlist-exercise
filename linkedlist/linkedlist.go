6c5343d011e732fd291a236879185e6ae1fb326d
Alexey Papulovskiy <a@c17.nl>
2015-11-10 22:31:51 +0100

    Looks like linked list is tested and implemented

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -13,9 +13,30 @@ type LinkedList struct {
 }
 
 func (l *LinkedList) Insert(v interface{}) {
-	l.Tail = Item{v, nil}
+	item := &Item{v, nil}
+	if l.Tail != nil {
+		l.Tail.next = item
+	}
+	if l.Head == nil {
+		l.Head = item
+	}
+	l.Tail = item
 }
 
-func (l *LinkedList) Contains() bool {
-
+func (l *LinkedList) Contains(v interface{}) bool {
+	cursor := l.Head
+	if cursor == nil {
+		return false
+	}
+	item := *cursor
+	for {
+		if item.Value == v {
+			return true
+		}
+		if item.next == nil {
+			return false
+		}
+		item = *item.next
+	}
+	return false
 }
--- linkedlist_test.go
+++ linkedlist_test.go
@@ -8,8 +8,34 @@ import (
 func TestInsert(t *testing.T) {
 	var ll linkedlist.LinkedList
 	ll.Insert(1)
+	ll.Insert("2")
 }
 
 func TestContains(t *testing.T) {
+	var ll linkedlist.LinkedList
+
+	if ll.Contains(1) {
+		t.Fatal("List should be empty")
+	}
+
+	ll.Insert(1)
+	if !ll.Contains(1) {
+		t.Fatal("List should contain element")
+	}
+
+	ll.Insert("2")
+	ll.Insert(3)
+	if !ll.Contains(1) {
+		t.Fatal("List should contain element")
+	}
+	if !ll.Contains("2") {
+		t.Fatal("List should contain element")
+	}
+	if !ll.Contains(3) {
+		t.Fatal("List should contain element")
+	}
+	if ll.Contains(4) {
+		t.Fatal("List should not contain element")
+	}
 
 }
